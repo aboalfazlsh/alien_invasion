c37d7efb5371d92fce7a5aabf609dc3add8737ce
aboalfazlsh <shokryaboalfazl@gmail.com>
2021-12-07 08:19:14 -0800

    create-screen

---
+++ alien_invasion.py
@@ -0,0 +1,20 @@
+import sys
+import pygame
+
+def run_game():
+    #initilize game and creat a screen object.
+    pygame.init()
+    screen=pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
+
+    #start the main loop for game .
+    while True:
+
+        #Watch for keyboard and mouse evevnts.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    # Make the most recently drawn screen visible
+    pygame.display.flip()
+
+run_game()
