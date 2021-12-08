02a8b03c69c94e0c19367eceafcc0ee9c63dca60
aboalfazlsh <shokryaboalfazl@gmail.com>
2021-12-08 02:31:45 -0800

    added class setting

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class to store all setting for Alien invasion."""
+
+
+    def __init__(self):
+        """initialize the games settings."""
+        #screen setting
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
