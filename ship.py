de3ce8355cd88d7076e4cb56dc0dc2a0e1e13d5a
aboalfazlsh <shokryaboalfazl@gmail.com>
2021-12-12 07:27:09 -0800

    fix error was not screen_rect

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship():
         #Load the ship image and get its rect.
         self.image=pygame.image.load("images/ship.bmp")
         self.rect=self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
         #Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
